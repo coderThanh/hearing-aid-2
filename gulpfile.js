2cb89491e9c0822052e3206da55da10917d28a48
coderThanh <nguyenvanthanh6499@gmail.com>
2025-10-28 13:52:40 +0700

    refactor: update component paths and improve gulp configuration
    - Comment out preloader in components and index.html
    - Update footer links and rename 'quick links' to 'Site map'
    - Correct component include paths in index.html
    - Add new blog component template
    - Optimize gulpfile.js by adjusting watch paths and removing unnecessary watches

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,53 @@
-import browserSync from "browser-sync";
-import fileInclude from "gulp-file-include";
-import gulp from "gulp";
+import browserSync from 'browser-sync'
+import fileInclude from 'gulp-file-include'
+import gulp from 'gulp'
 
-const server = browserSync.create();
+const server = browserSync.create()
 
-gulp.task("build", () => {
-  return gulp.src("src/pages/**/*.html")
-    .pipe(fileInclude({
-      prefix: "@@",
-      basepath: "src/components"
-    }))
-    .pipe(gulp.dest(".")); // build ra root
-});
+gulp.task('build', () => {
+  return gulp
+    .src('src/pages/**/*.html')
+    .pipe(
+      fileInclude({
+        prefix: '@@',
+        basepath: 'src/',
+      }),
+    )
+    .pipe(gulp.dest('.')) // build ra root
+})
 
-gulp.task("serve", () => {
+gulp.task('serve', () => {
   server.init({
-    server: { baseDir: "./" },
-    index: "index.html",
+    server: { baseDir: './' },
+    index: 'index.html',
     notify: false,
     open: false,
-    reloadOnRestart: true
-  });
+    reloadOnRestart: true,
+  })
 
   // Watch với callback để đảm bảo reload hoạt động đúng
-  gulp.watch("src/**/*.html", gulp.series("build")).on('change', () => {
-    server.reload();
-  });
-  
-  // custome code
-  gulp.watch("8pm-assest/css/**/*.css").on('change', () => {
-    server.reload();
-  });
-  gulp.watch("8pm-assest/js/**/*.js").on('change', () => {
-    server.reload();
-  });
-  
-  // root theme
-  gulp.watch("css/**/*.css").on('change', () => {
-    server.reload();
-  });
-  
-  gulp.watch("js/**/*.js").on('change', () => {
-    server.reload();
-  });
-});
+  //  If project have a lot file, to improve performance, we can watch only folder coding
+  gulp.watch('src/**/*.html', gulp.series('build')).on('change', () => {
+    server.reload()
+  })
 
-gulp.task("default", gulp.series("build", "serve"));
+  // custome code
+  gulp.watch('8pm-assest/css/**/*.css').on('change', () => {
+    server.reload()
+  })
+  gulp.watch('8pm-assest/js/**/*.js').on('change', () => {
+    server.reload()
+  })
+
+  // root theme
+  // We dont code at root theme so dont need to watch
+  // gulp.watch("css/**/*.css").on('change', () => {
+  //   server.reload();
+  // });
+
+  // gulp.watch("js/**/*.js").on('change', () => {
+  //   server.reload();
+  // });
+})
+
+gulp.task('default', gulp.series('build', 'serve'))
