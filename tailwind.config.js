b806c9da6a92b388072979818308caa05c6c3f08
coderThanh <nguyenvanthanh6499@gmail.com>
2025-10-30 20:34:00 +0700

    feat(gallery-page): add banner hero to GalleryPage
    - Comment out preloader HTML across multiple pages
    - Add new hero banner component to gallery page
    - Update tailwind config with new utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,12 @@ module.exports = {
   theme: {
     // connect with bootstrap screen size
     screens: {
-      'xs': '0px',      // Extra small devices (portrait phones)
-      'sm': '576px',    // Small devices (landscape phones) - Bootstrap sm
-      'md': '768px',    // Medium devices (tablets) - Bootstrap md  
-      'lg': '992px',    // Large devices (desktops) - Bootstrap lg
-      'xl': '1200px',   // Extra large devices (large desktops) - Bootstrap xl
-      'xxl': '1400px',  // Extra extra large devices - Bootstrap xxl
+      xs: '0px', // Extra small devices (portrait phones)
+      sm: '576px', // Small devices (landscape phones) - Bootstrap sm
+      md: '768px', // Medium devices (tablets) - Bootstrap md
+      lg: '992px', // Large devices (desktops) - Bootstrap lg
+      xl: '1200px', // Extra large devices (large desktops) - Bootstrap xl
+      xxl: '1400px', // Extra extra large devices - Bootstrap xxl
     },
     extend: {
       colors: {
@@ -32,21 +32,21 @@ module.exports = {
         warning: 'rgb(var(--color-warning) / <alpha-value>)',
         'text-title': 'rgb(var(--color-text-title) / <alpha-value>)',
         'text-basic': 'rgb(var(--color-text-basic) / <alpha-value>)',
-        'border': 'rgb(var(--color-border-1) / <alpha-value>)',
-        'bg': 'rgb(var(--bg) / <alpha-value>)',
+        border: 'rgb(var(--color-border-1) / <alpha-value>)',
+        bg: 'rgb(var(--bg) / <alpha-value>)',
         'bg-2nd': 'rgb(var(--bg-2nd) / <alpha-value>)',
         'bg-3nd': 'rgb(var(--bg-3nd) / <alpha-value>)',
         'bg-opposite': 'rgb(var(--bg-opposite) / <alpha-value>)',
-        'price': 'rgb(var(--color-price) / <alpha-value>)',
+        price: 'rgb(var(--color-price) / <alpha-value>)',
         'price-sale': 'rgb(var(--color-price-sale) / <alpha-value>)',
       },
       spacing: {
-        '1': '1px',
+        1: '1px',
       },
     },
     fontSize: {
       xs: '12px',
-      'sm': 'var(--size-small)',
+      sm: 'var(--size-small)',
       'size-sm': 'var(--size-small)',
       'size-xs': 'var(--size-small-a)',
       'size-1': 'var(--size-1)',
@@ -57,6 +57,14 @@ module.exports = {
       'size-6': 'var(--size-6)',
       'size-7': 'var(--size-7)',
       'size-8': 'var(--size-8)',
+    },
+    borderRadius: {
+      none: '0',
+      sm: 'var(--radius-sm)',
+      md: 'var(--radius-1)',
+      lg: 'var(--radius-2)',
+      xl: 'var(--radius-3)',
+      xxl: 'var(--radius-4)',
     },
   },
   plugins: [],
